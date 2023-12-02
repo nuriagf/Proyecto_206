26902e111f0c0c67c3818aaaf05b95497b8a3c6b
Nurii <i12gafen@uco.es>
2023-12-02 13:07:28 +0100

    Iniciar sesion funciona

---
+++ funciones.h
@@ -0,0 +1,12 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+bool validarUsuario(std::string& usuario, std::string& contrasena);
+bool inicioSesion(std::string& usuario, std::string& contrasena);
+bool comprobarUsuario(std:: string& usuario);
+
+#endif
