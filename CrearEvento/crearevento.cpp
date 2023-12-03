3b78f9840016d87f52302a6d701d7c64a16878dc
Blanca Lara <i12lanob@uco.es>
2023-12-03 23:55:34 +0100

    Ejemplos de eventos creados

--- CrearEvento/crearevento.cpp
+++ CrearEvento/crearevento.cpp
@@ -4,25 +4,52 @@ void crearEvento() {
     std:: string nombre, tipo, descripcion, fechaInicio, fechaFin; 
     int duracion; 
 
-    std::cout << "Introduzca el nombre del evento" << std::endl; 
+    std::cin.ignore();//limpiar buffer
+
+    std::cout << "Introduzca el nombre del evento (pulsa -enter- en una linea vacia para fianlizar)" << std::endl; 
     std::cin >> nombre;
+
+    while (true) {
+        std::string linea;
+        std::getline(std::cin, linea); 
+        
+        if (linea.empty()) { //cuando haya un enter en la linea vacia se saldrá 
+            break;
+        }
+
+        nombre += linea + " "; 
+    }
 
     if(comprobarEvento(nombre)) { //si el evento ya existe se sale
         return; 
     } 
 
-    std::cout << "Introduzca el tipo de evento" << std::endl; 
+    std::cout << "Introduzca el tipo de evento (congreso, ponencia, taller, seminario)" << std::endl; 
     std::cin >> tipo;
 
-    std::cout << "Introduzce una descripcion del evento" << std::endl; 
-    std::cin >> descripcion;
+    std::cout << "Introduzce una descripcion del evento (pulsa -enter- en una linea vacia para fianlizar)" << std::endl; 
 
+    std::cin.ignore();//limpiar buffer
+
+    while (true) {
+        std::string linea;
+        std::getline(std::cin, linea); 
+        
+        if (linea.empty()) { //cuando haya un enter en la linea vacia se saldrá 
+            break;
+        }
+
+        descripcion += linea + " "; 
+    }
+
+    //prueba de que fecha de inicio no puede ser después de fecha final 
     std::cout << "Introduzca fecha de inicio (AAAA-MM-DD)" << std::endl; 
     std::cin >> fechaInicio;
 
     std::cout << "Introduzca fecha de fin (AAAA-MM-DD)" << std::endl; 
     std::cin >> fechaFin;
 
+    //prueba de que no se pueda poner algo que no es de tipo entero
     std::cout << "Introduzca duracion del evento(en minutos)" << std::endl; 
     std::cin >> duracion;
 
@@ -53,7 +80,7 @@ bool comprobarEvento(std::string& nombre) {
 }
 
 bool guardarEvento(std::string& nombre, std::string& tipo, std::string& descripcion, std::string& fechaInicio, std::string& fechaFin, int duracion) {
-    std::ofstream archivo("eventos.txt", std::ios::app);
+    std::ofstream archivo("eventos.txt", std::ios::app); //añadir al final del fichero 
 
     if (!archivo.is_open()) {
         std::cerr << "Error al abrir el archivo de eventos." << std::endl;
@@ -66,6 +93,7 @@ bool guardarEvento(std::string& nombre, std::string& tipo, std::string& descripc
     archivo << "Fecha inicio: " << fechaInicio << std::endl; 
     archivo << "Fecha fin: " << fechaFin << std::endl; 
     archivo << "Duracion: " << duracion << std::endl; 
+    archivo << std::endl; 
     return true; 
 
     archivo.close();
